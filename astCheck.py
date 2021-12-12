439e50b5371d53cdd2f30965e7f62af3daa6e85a
heeking13 <513301743@qq.com>
2021-12-12 10:37:09 +0000

    update

--- astCheck.py
+++ astCheck.py
@@ -2,7 +2,8 @@
 ast checker to iterating over all the nodes
 '''
 
-import ast,_ast
+import ast
+import _ast
 import re
 import astunparse
 
@@ -14,58 +15,53 @@ class CodeVistor(ast.NodeVisitor):
         self.defmagic = set()
         self.result = []
 
-    def visit_FunctionDef(self,node):
-      #parameters count
-      def findCharacter(s,d):
-        try:
-          value = s.index(d)
-        except ValueError:
-          return -1
-        else:
-          return value
-      stmt = astunparse.unparse(node.args)
-      arguments = stmt.split(",")
-      argsCount = 0
-      for element in arguments:
-        if findCharacter(element,'=') == -1:
-          argsCount += 1
-      self.result.append((1,self.fileName,node.lineno,argsCount))
-      #function length
-      lines = set()
-      res = [node]
-      while len(res) >= 1:
-        t = res[0]
-        for n in ast.iter_child_nodes(t):
-          if not hasattr(n,'lineno') or ((isinstance(t,_ast.FunctionDef) or isinstance(t,_ast.ClassDef)) and n == t.body[0] and isinstance(n,_ast.Expr)):
-            continue
-          lines.add(n.lineno)
-          if isinstance(n,_ast.ClassDef) or isinstance(n,_ast.FunctionDef):
-            continue
-          else:
-            res.append(n)
-        del res[0]
-      self.result.append((2,self.fileName,node.lineno,len(lines))) 
-      self.generic_visit(node) 
+    def visit_FunctionDef(self, node):
+        # parameters count
+        def findCharacter(s, d):
+            try:
+                value = s.index(d)
+            except ValueError:
+                return -1
+            else:
+                return value
+        stmt = astunparse.unparse(node.args)
+        arguments = stmt.split(",")
+        argsCount = 0
+        for element in arguments:
+            if findCharacter(element, '=') == -1:
+                argsCount += 1
+        self.result.append((1, self.fileName, node.lineno, argsCount))
+        # function length
+        lines = set()
+        res = [node]
+        while len(res) >= 1:
+            t = res[0]
+            for n in ast.iter_child_nodes(t):
+                if not hasattr(n, 'lineno') or ((isinstance(t, _ast.FunctionDef) or isinstance(t, _ast.ClassDef)) and n == t.body[0] and isinstance(n, _ast.Expr)):
+                    continue
+                lines.add(n.lineno)
+                if isinstance(n, _ast.ClassDef) or isinstance(n, _ast.FunctionDef):
+                    continue
+                else:
+                    res.append(n)
+            del res[0]
+        self.result.append((2, self.fileName, node.lineno, len(lines)))
+        self.generic_visit(node)
 
-    def visit_ClassDef(self,node):
-      #large class
-      lines = set()
-      res = [node]
-      while len(res) >= 1:
-        t = res[0]
-        for n in ast.iter_child_nodes(t):
-          if not hasattr(n,'lineno') or ((isinstance(t,_ast.FunctionDef) or isinstance(t,_ast.ClassDef)) and n == t.body[0] and isinstance(n,_ast.Expr)):
-            continue
-          lines.add(n.lineno)
-          if isinstance(n,_ast.ClassDef) or isinstance(n,_ast.FunctionDef):
-            continue
-          else:
-            res.append(n)
-        del res[0]
-      self.result.append((3,self.fileName,node.lineno,len(lines)))
-      self.generic_visit(node)
-
-
-
-
-
+    def visit_ClassDef(self, node):
+        # large class
+        lines = set()
+        res = [node]
+        while len(res) >= 1:
+            t = res[0]
+            for n in ast.iter_child_nodes(t):
+                if not hasattr(n, 'lineno') or ((isinstance(t, _ast.FunctionDef) or isinstance(t, _ast.ClassDef)) and n == t.body[0] and isinstance(n, _ast.Expr)):
+                    continue
+                lines.add(n.lineno)
+                if isinstance(n, _ast.ClassDef) or isinstance(n, _ast.FunctionDef):
+                    continue
+                else:
+                    res.append(n)
+            del res[0]
+        self.result.append((3, self.fileName, node.lineno, len(lines)))
+        self.generic_visit(node)
--- detector.py
+++ detector.py
@@ -25,28 +25,28 @@ def detect_smells(path):
             if item[0] == 1:
                 if int(item[3])>=smells_metrics_thresholds['Long Parameter List'][0]:
                     detected_codesmells.append(
-                        [path, item[1], item[2], item[3], 'Long Parameter List'])
+                        [path, item[1], item[2], 'Long Parameter List'])
                     continue
             elif item[0] == 2:
                 if int(item[3])>=smells_metrics_thresholds['Long Method'][0]:
                     detected_codesmells.append(
-                        [path, item[1], item[2], item[3], 'Long Method'])
+                        [path, item[1], item[2], 'Long Method'])
                     continue
             elif item[0] == 3:
                 if int(item[3])>=smells_metrics_thresholds['Large Class'][0]:
                     detected_codesmells.append(
-                        [path, item[1], item[2], item[3], 'Large Class'])
+                        [path, item[1], item[2], 'Large Class'])
                     continue
     return detected_codesmells
 
 def main():
     # Add project absolute path
-    absolute_path = "/Users/heqing/Downloads/python-mini-projects-master"
+    absolute_path = "/Users/heqing/Downloads/flasky-master"
     codesmells = detect_smells(absolute_path)
     df = pd.DataFrame(codesmells)
     try:
         df.columns = ["Project path", "File name",
-                      "Line number", "Number of metrics", "Code smell name"]
+                      "Line number", "Code smell name"]
     except:
         print("There is no code smells in this project.")
     df.to_csv('codesmell.csv')   #save the df to the csv file
